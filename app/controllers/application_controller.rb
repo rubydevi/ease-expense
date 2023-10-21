6aedb14f26bf057322629b3f6ad87a2c974f7197
iamrubydevi <ariesabyss@gmail.com>
2023-10-21 17:17:19 +0530

    Authenticate app with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  helper_method :current_user
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+  end
 end
